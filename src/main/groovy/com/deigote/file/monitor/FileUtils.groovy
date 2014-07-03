2fdf4ab9c8ce5015c85465cb276c5106b87cfadf
deigote <diego@toharia.com>
2014-07-03 21:34:52 +0200

    Added a file utils class that will take care of finding directory paths refs #1

---
+++ src/main/groovy/com/deigote/file/monitor/FileUtils.groovy
@@ -0,0 +1,14 @@
+package com.deigote.file.monitor
+
+import java.nio.file.Path
+import java.nio.file.WatchService
+
+class FileUtils {
+
+   static Set<Path> findDirectoriesPaths(Collection<File> rootDirectories, Boolean recursive) {
+   }
+
+   static WatchService createWatchService() {
+   }
+
+}
