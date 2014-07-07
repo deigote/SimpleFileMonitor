39eec004579f08398dd14417b5a782bc8df7ed95
deigote <diego@toharia.com>
2014-07-07 23:41:33 +0200

    Created a method that gathers paths for the given root directories, invoking a "get sub paths" method or not according to its recursive parameter refs #1

--- src/main/groovy/com/deigote/file/monitor/FileUtils.groovy
+++ src/main/groovy/com/deigote/file/monitor/FileUtils.groovy
@@ -1,14 +1,28 @@
 package com.deigote.file.monitor
 
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
 import java.nio.file.WatchService
+import java.nio.file.attribute.BasicFileAttributes
+import static java.nio.file.FileSystems.getDefault as getDefaultFS
 
 class FileUtils {
 
    static Set<Path> findDirectoriesPaths(Collection<File> rootDirectories, Boolean recursive) {
+      Set<Path> rootPaths = rootDirectories*.getPath().collect { getDefaultFS().getPath(it) }
+      recursive ? rootPaths.inject([] as Set) { Set<Path> gatheredPaths, Path rootDirectoryPath ->
+         gatheredPaths + findAllSubPaths(rootDirectoryPath)
+      } : rootPaths
+   }
+
+   static Set<Path> findAllSubPaths(Path rootDirectoryPath) {
+      []
    }
 
    static WatchService createWatchService() {
+      getDefaultFS().newWatchService()
    }
 
 }
---
+++ src/test/groovy/com/deigote/file/monitor/FileUtilsSpec.groovy
@@ -0,0 +1,68 @@
+package com.deigote.file.monitor
+
+import com.deigote.misc.Logging
+import spock.lang.Specification
+import spock.lang.Unroll
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.WatchKey
+import java.nio.file.WatchService
+import java.nio.file.attribute.BasicFileAttributes
+
+class FileUtilsSpec extends Specification implements Logging {
+
+   void tearDown() {
+      TestUtils.instance.cleanMetaClasses FileSystems, FileSystem, Files, FileUtils
+   }
+
+   @Unroll
+   void "when invoked #recursiveLabel, the method findDirectoriesPaths will collect the paths of the given root directories #recursiveCallLabel"() {
+      given: 'A collection of files'
+      List<File> files = rawPaths.collect { Mock(File) }.eachWithIndex { File entry, int i ->
+         entry.getPath() >> rawPaths[i]
+      }
+
+      and: 'A working default file system'
+      List<String> rawPathsWhosePathHasBeenAskedFor = []
+      List<Path> returnedPaths = [], pathsWhoseSubPathsHasBeenAskedFor = [], returnedSubpaths = []
+      def mockedDefaultFS = FileSystems.getDefault()
+      mockedDefaultFS.metaClass.getPath = { String rawPath ->
+         rawPathsWhosePathHasBeenAskedFor << rawPath
+         returnedPaths.add(Mock(Path))
+         return returnedPaths.last()
+      }
+      GroovyMock(FileSystems, global:true)
+      FileSystems.getDefault() >> mockedDefaultFS
+
+      and: 'A method to obtain all subpaths of a given path'
+      FileUtils.metaClass.'static'.findAllSubPaths = { Path path ->
+         pathsWhoseSubPathsHasBeenAskedFor.add(path)
+         returnedSubpaths << Mock(Path)
+         return [path, returnedSubpaths.last()] as Set
+      }
+
+      when: "findDirectoriesPaths is invoked ${recursiveLabel}"
+      def obtainedPaths = FileUtils.findDirectoriesPaths(files, recursiveArg)
+
+      then: 'all the files has been asked for their path'
+      rawPathsWhosePathHasBeenAskedFor.size() == rawPaths.size()
+      rawPathsWhosePathHasBeenAskedFor.every { it in rawPaths }
+
+      and: 'the paths returned by the file system, and the subpaths when recursive, are returned'
+      obtainedPaths == (returnedPaths as Set) + (returnedSubpaths as Set)
+
+      and: 'When recursive, all the paths has been asked for their subpaths and those are part of the result'
+      pathsWhoseSubPathsHasBeenAskedFor == (recursiveArg ? returnedPaths : [])
+
+      where:
+      rawPaths = ['/aFile', '/anotherFile', '/aFile/aSubfile']
+
+      recursiveArg | recursiveLabel     | recursiveCallLabel
+      true         | 'as recursive'     | 'and all its subpaths'
+      false        | 'as non recursive' | ''
+
+   }
+
+}
